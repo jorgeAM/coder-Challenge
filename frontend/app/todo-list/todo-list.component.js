818309ff97daff41daddf114c8fa61b07e7da5c1
Jorge L <jorge.alfmur@gmail.com>
2020-06-19 16:08:53 -0500

    input to add todo

--- frontend/app/todo-list/todo-list.component.js
+++ frontend/app/todo-list/todo-list.component.js
@@ -1,27 +1,39 @@
 angular.module('todoList').component('todoList', {
   template: `
-    <ul>
-      <li class="todo" ng-repeat="todo in $ctrl.todos">
-        <div class="title-todo">
-          <input
-            class="form-check-input"
-            type="checkbox"
-            value=""
-          />
-          <span class="left">{{todo.title}}</span>
-        </div>
-        <button type="button" class="btn btn-danger">Borrar</button>
-      </li>
-    </ul>
+    <header class="header">
+      <h1>Tienes <span class="emphasis">{{$ctrl.todos.length}}</span> cosas por hacer</h1>
+    </header>
+    <section>
+      <article>
+        <ul>
+          <li class="todo" ng-repeat="todo in $ctrl.todos">
+            <div class="title-todo">
+              <input
+                class="form-check-input"
+                type="checkbox"
+                value=""
+              />
+              <span class="left">{{todo.title}}</span>
+            </div>
+            <button type="button" class="btn btn-danger">Borrar</button>
+          </li>
+        </ul>
+      </article>
+    </section>
+    <section>
+      <article class="add-todo-container">
+        <input class="add-todo-input" ng-model="$ctrl.query"/>
+        <input class="add-todo-btn" type="submit" value="AGREGAR"/>
+      </article>
+    </section>
+    <p>{{$ctrl.query}}</p>
   `,
   controller: function TodoListController() {
     this.todos = [
       {
         title: 'Ir al mercado'
-      },
-      {
-        title: 'Cortarme el cabello'
       }
     ]
+    this.query = ''
   }
 })
