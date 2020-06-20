306645043d3a07d41623793ccb62d99a72c6718b
Jorge L <jorge.alfmur@gmail.com>
2020-06-19 21:06:17 -0500

    edit todo

--- frontend/app/todo-list/todo-list.component.js
+++ frontend/app/todo-list/todo-list.component.js
@@ -16,7 +16,16 @@ angular.module('todoList').component('todoList', {
                 ng-model="todo.done"
                 ng-change="$ctrl.doneTodo(todo._id)"
               />
-              <span ng-class="{done: todo.done}" class="left">{{todo.title}}</span>
+              <span
+                id="{{todo._id}}"
+                ng-class="{done: todo.done}"
+                class="left"
+                contenteditable
+                ng-model="todo.title"
+                ng-blur="$ctrl.updateTodo(todo._id)"
+              >
+                {{todo.title}}
+              </span>
             </div>
             <button
               type="button"
@@ -51,6 +60,12 @@ angular.module('todoList').component('todoList', {
 
     this.addTodo = function addTodo() {
       const title = this.title
+
+      if (title === '') {
+        alert('Escribe una tarea!!')
+        return
+      }
+
       $http.post('http://localhost:9000/api/v1/todo', { title }).then(res => {
         const todo = res.data.todo
         this.todos.push(todo)
@@ -60,13 +75,22 @@ angular.module('todoList').component('todoList', {
     }
 
     this.deleteTodo = function deleteTodo(id) {
-      if (!confirm('Are you sure you wanna remove this todo?')) {
+      if (!confirm('Estas seguro que quieres quitas esta tarea?')) {
         return
       }
 
       $http.delete(`http://localhost:9000/api/v1/todo/${id}`).then(() => {
         this.todos = this.todos.filter(todo => todo._id !== id)
       })
+    }
+
+    this.updateTodo = function updateTodo(id) {
+      const el = document.getElementById(id)
+      const title = el.innerHTML.trim()
+
+      $http
+        .put(`http://localhost:9000/api/v1/todo/${id}`, { title })
+        .then(() => console.log(`todo ${id} was updated`))
     }
 
     this.doneTodo = function doneTodo(id) {
